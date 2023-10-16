3b1b94c58114840bc2ce73280adcdd9c1d527a69
annSolstice <117833595+annSolstice@users.noreply.github.com>
2023-10-16 20:27:43 +0700

    Update header.php

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -1,77 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Halaman <?= $data['judul']; ?> </title>
-    <link rel="stylesheet" href="<?= BASEURL; ?> /css/bootstrap.css">
-    <!-- Vendor CSS Files -->
-    <link href="<?= BASEURL; ?>/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-    <link href="<?= BASEURL; ?>/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?= BASEURL; ?>/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="<?= BASEURL; ?>/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="<?= BASEURL; ?>/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-    <link href="<?= BASEURL; ?>/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-    <link href="<?= BASEURL; ?>/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-    <!-- Template Main CSS File -->
-    <link href="<?= BASEURL; ?>/assets/css/style.css" rel="stylesheet">
+<head>
+  <meta charset="UTF-8">
+  <title>Halaman <?= $data['judul']; ?> </title>
+  <link rel="stylesheet" href="<?= BASEURL; ?> /css/bootstrap.css">
+  <!-- Template Main CSS File -->
+  <link href="<?= BASEURL; ?>/assets/css/style.css" rel="stylesheet">
+  <link href="<?= BASEURL; ?>/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="<?= BASEURL; ?>/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?= BASEURL; ?>/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?= BASEURL; ?>/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
 </head>
+
 <body>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid mt-1">
+
+  <nav class="navbar navbar-expand-lg" style="background-color: sandybrown;">
+  <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light"> -->
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="<?= BASEURL; ?>/img/logoUSD-removebg.png" height="50" />
+      </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <nav class="navbar navbar-light bg-light">
-    <div class="container">
-      <!-- <a class="navbar-brand" href="#"> -->
-        <img src="<?= BASEURL; ?>/img/logoUSD.jpg" height="80"alt=""/>
-       <!-- </a> -->
-    </div>
-  </nav>
+
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="container mt-1">
-          <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="<?= BASEURL; ?>/home/indexLogin">Home</a>
-            <div class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="perekamanDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Sarana Dan Prasarana</a>
-              <ul class="dropdown-menu" aria-labelledby="perekamanDropdown">
-                <?php if ($_SESSION['role'] === 'BIRO' || $_SESSION['role'] === 'SEKRE') : ?>
-                  <li><a class="dropdown-item" href="<?= BASEURL; ?>/perekaman/ruang">Ruang</a></li>
-                  <li><a class="dropdown-item" href="<?= BASEURL; ?>/perekaman/barang">Barang</a></li>
-                  <?php endif; ?>
-                  <?php if($_SESSION['role'] === 'MAHASISWA') :?> 
-                  <li><a class="dropdown-item" href="<?= BASEURL; ?>/perekaman/tabelRuang">Ruang</a></li>
-                  <li><a class="dropdown-item" href="<?= BASEURL; ?>/perekaman/tabelBarang">Barang</a></li>
-                  <?php endif; ?>
-              </ul>
-            </div>
-            <div class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="permintaanDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Peminjaman</a>
-              <ul class="dropdown-menu" aria-labelledby="permintaanDropdown">
-                  <li><a class="dropdown-item" href="<?= BASEURL; ?>/permintaan/status">Status Permintaan</a></li>
-                  <?php if ($_SESSION['role'] === 'MAHASISWA'):?>
-                  <li><a class="dropdown-item" href="<?= BASEURL; ?>/permintaan/tambah">Perekaman Permintaan</a></li>
-                  <?php endif;?>
-                  <?php if ($_SESSION['role'] === 'SEKRE'):?>
-                  <li><a class="dropdown-item" href="<?= BASEURL; ?>/permintaan/verif">Verifikasi</a></li>
-                  <?php endif;?>
-                  <?php if ($_SESSION['role'] === 'BIRO'):?>
-                  <li><a class="dropdown-item" href="<?= BASEURL; ?>/permintaan/konfirmasi">Konfirmasi</a></li>
-                  <?php endif;?>
-              </ul>
-            </div>
-            <a class="nav-link active" aria-current="page" href="<?= BASEURL; ?>/home/logout">Logout</a>
-<!--             <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form> -->
+        <div class="navbar-nav me-auto mb-1 mb-lg-0">
+          <a class="nav-link active" aria-current="page" href="<?= BASEURL; ?>/home/indexLogin">Home</a>
+          <div class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="perekamanDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: black">Sarana Dan Prasarana</a>
+            <ul class="dropdown-menu" aria-labelledby="perekamanDropdown" >
+              <?php if ($_SESSION['role'] === 'BIRO' || $_SESSION['role'] === 'SEKRE') : ?>
+                <li><a class="dropdown-item" href="<?= BASEURL; ?>/perekaman/ruang">Ruang</a></li>
+                <li><a class="dropdown-item" href="<?= BASEURL; ?>/perekaman/barang">Barang</a></li>
+              <?php endif; ?>
+              <?php if ($_SESSION['role'] === 'MAHASISWA') : ?>
+                <li><a class="dropdown-item" href="<?= BASEURL; ?>/perekaman/tabelRuang">Ruang</a></li>
+                <li><a class="dropdown-item" href="<?= BASEURL; ?>/perekaman/tabelBarang">Barang</a></li>
+              <?php endif; ?>
+            </ul>
           </div>
+          <div class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="permintaanDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: black">Peminjaman</a>
+            <ul class="dropdown-menu" aria-labelledby="permintaanDropdown">
+              <li><a class="dropdown-item" href="<?= BASEURL; ?>/permintaan/status">Status Permintaan</a></li>
+              <?php if ($_SESSION['role'] === 'MAHASISWA') : ?>
+                <li><a class="dropdown-item" href="<?= BASEURL; ?>/permintaan/tambah">Perekaman Permintaan</a></li>
+              <?php endif; ?>
+              <?php if ($_SESSION['role'] === 'SEKRE') : ?>
+                <li><a class="dropdown-item" href="<?= BASEURL; ?>/permintaan/verif">Verifikasi</a></li>
+              <?php endif; ?>
+              <?php if ($_SESSION['role'] === 'BIRO') : ?>
+                <li><a class="dropdown-item" href="<?= BASEURL; ?>/permintaan/konfirmasi">Konfirmasi</a></li>
+              <?php endif; ?>
+            </ul>
+          </div>
+          <a class="nav-link active" aria-current="page" href="<?= BASEURL; ?>/home/logout">Logout</a>
         </div>
+
+        <div class="social-links d-none d-md-flex" style="padding-right: 50px;">
+          <a href="https://www.usd.ac.id/fakultas/sainsdanteknologi/" class="facebook"><i class="bi bi-globe"></i></a>
+          <a href="https://www.instagram.com/fstsanatadharma/" class="instagram"><i class="bi bi-instagram"></i></a>
+          <a href="https://www.youtube.com/@kpufstusd5012" class="youtube"><i class="bi bi-youtube"></i></i></a>
+        </div>
+
       </div>
     </div>
   </nav>
-  
-  <script src="<?= BASEURL; ?>/js/bootstrap.bundle.js"></script>
-</body>
-</html>
