40d654f3becbf5f37e10bdadcd8c1fb62287ce9e
Marta028 <126174251+Marta028@users.noreply.github.com>
2023-10-18 12:16:20 +0700

    Update Model_PerekamanB.php

--- app/models/Model_PerekamanB.php
+++ app/models/Model_PerekamanB.php
@@ -17,13 +17,15 @@ class Model_PerekamanB {
         $this->db->bind('kodeB', $kodeB);
         return $this->db->single();
     }
-    public function tambahDataB($data){
-        $query = "INSERT INTO `barang`(`kodeB`, `namaB`) VALUES (:kodeB, :namaB)";
     
+    public function tambahDataB($data, $Foto) {
+        $query = "INSERT INTO $this->table (kodeB, namaB, foto) VALUES (:kodeB, :namaB, :foto)";
+        
         $this->db->query($query);
         $this->db->bind('kodeB', $data['kodeB']);
         $this->db->bind('namaB', $data['namaB']);
-    
+        $this->db->bind('foto', $Foto); // Simpan nama file foto
+        
         $this->db->execute();
         return $this->db->rowCount(); 
     }
@@ -36,18 +38,7 @@ class Model_PerekamanB {
         $this->db->execute();
         return $this->db->rowCount();
     }
-    public function ubahDataB($data)
-    {
-        $query = "UPDATE barang SET kodeB = :kodeB, namaB = :namaB WHERE kodeB = :kodeB";
 
-        $this->db->query($query);
-        $this->db->bind('kodeB', $data['kodeB']);
-        $this->db->bind('namaB', $data['namaB']);
-
-        $this->db->execute();
-
-        return $this->db->rowCount();
-    }
     public function cariDataB()
     {
         $keyword = $_POST['keyword'];
@@ -55,6 +46,58 @@ class Model_PerekamanB {
         $this->db->query($query);
         $this->db->bind('keyword', "%$keyword%");
         return $this->db->resultSet();
+    }
+    // Fungsi untuk mengunggah foto
+    public function unggahFoto() {
+        $Foto = $_FILES['foto']['name'];
+        $ukuranFile = $_FILES['foto']['size'];
+        $error = $_FILES['foto']['error'];
+        $tmpName = $_FILES['foto']['tmp_name'];
+
+        $tujuan = 'foto/' . $Foto;
+
+        if (move_uploaded_file($tmpName, $tujuan)) {
+            // Jika berhasil diunggah, kembalikan nama file asli
+            return $Foto;
+        } else {
+            // Jika gagal diunggah, Anda dapat menangani kesalahan di sini
+            return false;
+        }
+    }
+
+    public function ubahDataB($data)
+    {
+        // Ambil data nama dan kode barang yang diubah
+        $namaB = $data['namaB'];
+        $kodeB = $data['kodeB'];
+
+        // Ambil data foto lama
+        $query = "SELECT foto FROM barang WHERE kodeB = :kodeB";
+        $this->db->query($query);
+        $this->db->bind('kodeB', $kodeB);
+        $fotoLama = $this->db->single()['foto'];
+
+        // Proses unggah foto baru
+        if (!empty($_FILES['foto']['name'])) {
+            // Hapus foto lama jika ada
+            if (!empty($fotoLama)) {
+                unlink('public/foto/' . $fotoLama);
+            }
+            // Menggunakan fungsi unggahFoto() yang sudah ada
+            $fotoBaru = $this->unggahFoto();
+        } else {
+            $fotoBaru = $fotoLama; // Gunakan foto lama jika tidak ada foto yang diunggah
+        }
+
+        // Update data termasuk foto baru
+        $query = "UPDATE barang SET kodeB = :kodeB, namaB = :namaB, foto = :foto WHERE kodeB = :kodeB";
+        $this->db->query($query);
+        $this->db->bind('kodeB', $data['kodeB']);
+        $this->db->bind('namaB', $data['namaB']);
+        $this->db->bind('foto', $fotoBaru); // Perbaikan di sini
+        $this->db->execute();
+
+        return $this->db->rowCount();
     }
 }
 ?>
