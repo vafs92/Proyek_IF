b75d4ecb04f9276001b6d28b7e121d036bce4755
Marta028 <126174251+Marta028@users.noreply.github.com>
2023-10-20 15:20:01 +0700

    Update Detail.php

--- app/controllers/Detail.php
+++ app/controllers/Detail.php
@@ -14,5 +14,20 @@ class Detail extends Controller {
 
         $this->view('detail/showR', ['ruang' => $ruang]);
     }
+
+    public function showTB($kodeB) {
+        $barangModel = $this->model('Model_PerekamanB');
+        $barang = $barangModel->getPerekamanByKodeB($kodeB); 
+
+        $this->view('detail/showTB', ['barang' => $barang]);
+    }
+
+    public function showTR($kodeR) {
+        $ruangModel = $this->model('Model_PerekamanR');
+        $ruang = $ruangModel->getPerekamanByKodeR($kodeR); 
+
+        $this->view('detail/showTR', ['ruang' => $ruang]);
+    }
 }
 ?>
+
